dec108c4f124a4b52f09e7dadc30fd06d5809982
Andrew Clunis <andrew@orospakr.ca>
2008-02-23 04:44:52 -0500

    Added ConfigParser for --port, and fixed up stories.

--- ikarus/ikarusd.py
+++ ikarus/ikarusd.py
@@ -2,13 +2,20 @@
 
 import logging
 from twisted.internet import reactor
+from optparse import OptionParser
+import sys
 
 import ikarus.irc
 
 def run():
     logging.basicConfig(level=logging.DEBUG)
-    reactor.listenTCP(6667, ikarus.irc.IRCFactory())
-    logging.info("Ikarus started.  Listening...")
+    parser = OptionParser()
+    parser.add_option("-p", "--port", type="int", dest="port",
+                      help="TCP port to listen on.  May only be specified once. (default: %default)",
+                      default=6667)
+    (options, args) = parser.parse_args()
+    reactor.listenTCP(options.port, ikarus.irc.IRCFactory())
+    logging.info("Ikarus started.  Listening on port %i..." % options.port)
     reactor.run()
 
 if __name__ == '__main__':
